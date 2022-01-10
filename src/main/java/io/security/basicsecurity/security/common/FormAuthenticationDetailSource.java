b9a07232f557eadf09bb51d4c9b2785d24f595a7
dreamhandg <handg0972@gmail.com>
2022-01-10 21:00:54 +0900

    [edu_3-8] 인증 부가 기능 추가

---
+++ src/main/java/io/security/basicsecurity/security/common/FormAuthenticationDetailSource.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity.security.common;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class FormAuthenticationDetailSource implements AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
+    @Override
+    public WebAuthenticationDetails buildDetails(HttpServletRequest context) {
+        return new FormWebAuthenticationDetails(context);
+    }
+}
--- src/main/java/io/security/basicsecurity/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/io/security/basicsecurity/security/provider/CustomAuthenticationProvider.java
@@ -1,9 +1,12 @@
-package io.security.basicsecurity.security.configs.provider;
+package io.security.basicsecurity.security.provider;
 
-import io.security.basicsecurity.security.configs.service.AccountContext;
+import io.security.basicsecurity.security.common.FormWebAuthenticationDetails;
+import io.security.basicsecurity.security.service.AccountContext;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -18,6 +21,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    // 정책에 따라서 모든 인증에 대한 검증은 위한 로직을 추가 및 처리할수 있음
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
@@ -29,7 +33,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             throw new BadCredentialsException("BadCredentialsException");
         }
 
-        // 정책에 따라서 모든 인증에 대한 검증은 위한 로직을 추가 및 처리할수 있음
+        // WebAuthenticationDetails 예제 (로그인 폼에서 추가 파라미터 정보)
+        FormWebAuthenticationDetails formWebAuthenticationDetails = (FormWebAuthenticationDetails) authentication.getDetails();
+        String secretKey = formWebAuthenticationDetails.getSecretKey();
+
+        if(secretKey == null || !StringUtils.equals("secret", secretKey)) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
+        }
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
 
